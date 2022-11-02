f7ed489f0c46ab75e339f34350131f416d948699
GulamMdKhalid <80089845+GulamMd@users.noreply.github.com>
2022-11-03 00:41:23 +0530

    Added mongoDB connection into REST

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const colors = require('colors');
 const dotenv = require("dotenv").config();
 const { errorHandler } = require('./middleware/errorMiddleware');
-
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
@@ -15,3 +18,4 @@ app.use('/api/goals', require('./routes/goalRoutes'));
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
+//To run this server using nodemon type : npm run server
